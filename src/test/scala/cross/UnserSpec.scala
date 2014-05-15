79274bf788b6b8585fac4871cbfcd25414c5b090
giabao <giabao@sandinh.net>
2014-05-15 23:16:55 +0700

    add cross-languages test. Note: To run those suite, we need install php-cli. If running in windows -> need change path $dataFile in data.php & change dataFile in Data.scala

---
+++ src/test/scala/cross/UnserSpec.scala
@@ -0,0 +1,38 @@
+package cross
+
+import com.sandinh.PhpObject.parse
+import java.io.{BufferedInputStream, FileInputStream}
+import org.scalatest.FunSuite
+import sys.process._
+
+class UnserSpec extends FunSuite {
+  def unser(): Any = {
+    val bis = new BufferedInputStream(new FileInputStream(Data.dataFile))
+    val bytes = Stream.continually(bis.read).takeWhile(_ != -1).map(_.toByte).toArray
+    val ret = parse(bytes)
+    bis.close()
+    ret
+  }
+
+  test("ser from PHP should be validated in PHP unser") {
+    "php src/test/php/ser.php".!!
+
+    //if php test success => exit code == 0
+    //else => exit code == 1
+    // => "RuntimeException: Nonzero exit value: 1"
+    // => test fail
+    "php src/test/php/unser.php".!!
+  }
+
+  test("ser from PHP should be validated in SCALA unser") {
+    "php src/test/php/ser.php".!!
+    unser() match {
+      case result: Map[_, _] =>
+        assert(result.values.dropRight(1).sameElements(Data.data.dropRight(1)))
+        val dummyActual = result.values.last
+        assert(dummyActual == Data.dummyExpected)
+
+      case _ => fail()
+    }
+  }
+}
